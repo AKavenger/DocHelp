2f631572acd5dfe4ee117a56dce222e1a0d85a78
Avinash Kushwaha <avinashkushwaha2001@gmail.com>
2022-12-04 04:17:20 +0530

    deploy changes

--- server.js
+++ server.js
@@ -12,12 +12,11 @@ app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
-// Heroku build configuration
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+// CYLIC build configuration
+app.use("/", express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
 const port = process.env.PORT || 5000;
