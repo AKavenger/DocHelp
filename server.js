2f8e827b655115c181bb3ffeec4c6ce0f366b862
Avinash Kushwaha <avinashkushwaha2001@gmail.com>
2022-12-04 04:26:12 +0530

    Fixed an issue

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use("/", express.static("client/build"));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
-}
+});
+
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
