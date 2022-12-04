6ff5167a01c6235b0ebaa02f3c69c437f1eb1e90
Avinash Kushwaha <avinashkushwaha2001@gmail.com>
2022-12-04 15:16:20 +0530

    Fix8

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
-
+require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 app.use(express.json()); //To destructure the email and password
 const userRoute = require("./routes/userRoute");
@@ -13,13 +12,14 @@ app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
-// CYLIC build configuration
-app.use("/", express.static("client/build"));
+// Heroku build configuration
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
