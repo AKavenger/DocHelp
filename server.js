f41b33e87f627726c6debe25937d964c885c6b61
Avinash Kushwaha <avinashkushwaha2001@gmail.com>
2022-12-04 15:03:24 +0530

    Fix6

--- server.js
+++ server.js
@@ -14,17 +14,13 @@ app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
 // CYLIC build configuration
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
+app.use("/", express.static("client/build"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
 
-//app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Node Express Server Started at ${port}!`));
